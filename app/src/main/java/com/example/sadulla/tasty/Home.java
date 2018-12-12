168f6273cc6ab86f65cca3ad535b1d09ee7cb2b5
Witcher1993 <sashkasystem777@gmail.com>
2018-12-12 12:49:25 +0500

    12 Dec 12 49

--- app/src/main/java/com/example/sadulla/tasty/Home.java
+++ app/src/main/java/com/example/sadulla/tasty/Home.java
@@ -1,5 +1,6 @@
 package com.example.sadulla.tasty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -39,6 +40,8 @@ public class Home extends AppCompatActivity
     //
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
+
+    FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
 
 
 
@@ -92,7 +95,7 @@ public class Home extends AppCompatActivity
     }
 
     private void loadMenu() {
-        FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class, R.layout.menu_item, MenuViewHolder.class,category) {
+         adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class, R.layout.menu_item, MenuViewHolder.class,category) {
             @Override
             protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
                 viewHolder.menuName.setText(model.getName());
@@ -102,7 +105,14 @@ public class Home extends AppCompatActivity
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View v, int position, boolean isLongClick) {
-                        Toast.makeText(Home.this, ""+clickItem.getName(), Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(Home.this, ""+clickItem.getName(), Toast.LENGTH_SHORT).show();
+                        //Get CategoryId and send to another Activity
+                        Intent foodList = new Intent(Home.this, FoodList.class);
+                        //CategoryId is a key we just get key of this item
+                        foodList.putExtra("CategoryId", adapter.getRef(position).getKey());
+                        startActivity(foodList);
+
+
                     }
                 });
             }
---
+++ app/src/main/java/com/example/sadulla/tasty/ViewHolder/FoodViewHolder.java
@@ -0,0 +1,39 @@
+package com.example.sadulla.tasty.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sadulla.tasty.Interface.ItemClickListener;
+import com.example.sadulla.tasty.R;
+
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView foodName;
+    public ImageView food_image;
+
+    private ItemClickListener itemClickListener;
+
+
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public FoodViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        foodName = (TextView)itemView.findViewById(R.id.food_name);
+        food_image = (ImageView)itemView.findViewById(R.id.food_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view, getAdapterPosition(), false);
+
+    }
+}
