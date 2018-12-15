aaf722b8565e2b2a275936e88b22c625ac763af5
Witcher1993 <sashkasystem777@gmail.com>
2018-12-15 12:40:44 +0500

    15-dec-12:40

--- app/src/main/java/com/example/sadulla/tasty/OrderStatus.java
+++ app/src/main/java/com/example/sadulla/tasty/OrderStatus.java
@@ -2,8 +2,10 @@ package com.example.sadulla.tasty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.sadulla.tasty.Common.Common;
 import com.example.sadulla.tasty.Model.Request;
 import com.example.sadulla.tasty.ViewHolder.OrderViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -30,6 +32,40 @@ public class OrderStatus extends AppCompatActivity {
         requests = database.getReference("Requests");
 
         recyclerView = (RecyclerView)findViewById(R.id.oder_list_recycler);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        loadOrders(Common.currentUser.getPhone());
+
+    }
+
+    private void loadOrders(String phone) {
+        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(
+                Request.class,
+                R.layout.order_layout,
+                OrderViewHolder.class,
+                requests.orderByChild("phone").equalTo(phone)
+        ) {
+            @Override
+            protected void populateViewHolder(OrderViewHolder viewHolder, Request model, int position) {
+                viewHolder.orderIdTextView.setText(adapter.getRef(position).getKey());
+                viewHolder.orderStatusTextView.setText(convertCodeToStatus(model.getStatus()));
+                viewHolder.orderAddressTextView.setText(model.getAddress());
+                viewHolder.orderPhoneTextView.setText(model.getPhone());
+
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    private String convertCodeToStatus(String status) {
+        if(status.equals("0"))
+            return "Placed";
+        else if(status.equals("1"))
+            return "On Way";
+        else
+            return "Shipped";
 
     }
 }
